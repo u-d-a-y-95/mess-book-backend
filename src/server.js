cc66dbc682bcf5c2eac3a69e5c6fcabdb01310de
UDay <uday.dev.bd@gmail.com>
2022-07-03 22:37:39 +0600

    env config change

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ server.listen(process.env.PORT || 4000, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5000",
+    origin: process.env.CLIENT_BASE_URL || "http://localhost:5000",
     methods: ["GET", "POST"],
   },
 });
